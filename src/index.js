810c7e2af2781b1a962b870a47cc10e8c38bc1a9
Cody Delzer <delzercody@gmail.com>
2023-04-19 09:48:13 -0400

    completed via follow along

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 });
+
+// user clicks submit button
+
+//make a button
+
+//create an event listener
+
+let form = document.getElementById("create-task-form");
+createNewTask = form.addEventListener("submit", e => {;
+   //console.log(e)
+   e.preventDefault();
+ task = e.target['new-task-description'].value
+ //console.log(task)  
+
+ let liTask = document.createElement("li");
+ liTask.textContent = task;
+ console.log(liTask)
+
+  
+ let createdList = document.getElementById("list");
+ createdList.appendChild(liTask);
+})
+
+// Create a new element
+//set inner text to be task
+
+
+// get the ul
+// append new element to ul
